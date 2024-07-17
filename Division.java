d19ba21c2499541c2024236e2c161929e9856b2c
Riyasingh0808 <167004426+Riyasingh0808@users.noreply.github.com>
2024-07-18 00:07:34 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,15 @@
+package technicalLec1;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 3;
+		
+		float res = (float) num1/num2;
+		
+		System.out.println(res);
+
+	}
+
+}
---
+++ Loops.java
@@ -0,0 +1,14 @@
+package technicalLec1;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 30 ;i++) {
+			if(i%2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
